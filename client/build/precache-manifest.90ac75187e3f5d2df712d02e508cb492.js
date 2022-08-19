2f6bfd19ee78fdbcd66aef48c0bdc7a436c7ca07
Deepakkumarsen <deepak.19je0279@ece.iitism.ac.in>
2022-08-19 12:10:32 +0530

    first commit

---
+++ client/build/precache-manifest.90ac75187e3f5d2df712d02e508cb492.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a376f823ff262594eb359a9bc5cba751",
+    "url": "/index.html"
+  },
+  {
+    "revision": "747b832a0a3b966ddd12",
+    "url": "/static/css/main.9516a8fc.chunk.css"
+  },
+  {
+    "revision": "0bbff2121bebd84d5be1",
+    "url": "/static/js/2.efc62632.chunk.js"
+  },
+  {
+    "revision": "cd94619111e2ab78ce54eac27b302772",
+    "url": "/static/js/2.efc62632.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "747b832a0a3b966ddd12",
+    "url": "/static/js/main.37cd0d99.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.37cd0d99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f620b634b443fc7d2dfb",
+    "url": "/static/js/runtime-main.7e6e88a6.js"
+  },
+  {
+    "revision": "dc8bbfe30b9b1cf8867a83e69d051651",
+    "url": "/static/media/memoriesLogo.dc8bbfe3.png"
+  },
+  {
+    "revision": "2b89c5fae2d163d5a126a519646883fa",
+    "url": "/static/media/memoriesText.2b89c5fa.png"
+  }
+]);
